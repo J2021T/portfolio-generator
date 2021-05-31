826e0234a7f7ae9dc3cf7093799f401b0c66c83b
J2021T <80201567+J2021T@users.noreply.github.com>
2021-05-31 10:50:12 -0400

    added style and increase readability with asynchronous code

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-
-
-// const pageHTML = generatePage();
-
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Page created! Check out index.html to see the output!');
-// });
-
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 
 const promptUser = () => {
@@ -188,25 +178,23 @@ const promptProject = portfolioData => {
     ]
   };
 
-  const pageHTML = generatePage(mockData);
-
-
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw err;
-
-    console.log('Page created! Check out index.html to see the output!');
-});
 
   
-// promptUser()
-//     .then(promptProject)
-//     .then(portfolioData => {
-//         const pageHTML = generatePage(portfolioData);
-
-
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //     if (err) throw new Error(err);
-
-        //     console.log('Page created! Check out index.html to see the output!');
-        // });
-    // });
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
